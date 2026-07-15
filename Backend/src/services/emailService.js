f33836b30dc44280bf607d800d3e50bccee898fb
Chirag Greed <chiraggreed02@gmail.com>
2026-07-15 23:55:42 +0530

    refactor: replace nodemailer with @sendgrid/mail for email service
    - Removed nodemailer and nodemailer-sendgrid-transport dependencies.
    
    - Integrated @sendgrid/mail for sending emails.

--- Backend/src/services/emailService.js
+++ Backend/src/services/emailService.js
@@ -1,37 +1,22 @@
-import nodemailer from 'nodemailer';
-import sgTransport from 'nodemailer-sendgrid-transport';
-
-
-const tranporter = nodemailer.createTransport(
-    sgTransport({
-        auth: process.env.SENDGRID_API_KEY
-    })
-)
-
-
-tranporter.verify((error, success) => {
-    if (error) {
-        console.log('Error connecting to email server:', error)
-    }
-    else {
-        console.log("Email server is ready to send messages")
-    }
-})
-
+import sgMail from '@sendgrid/mail';
 
 
 const sendEmail = async (to, subject, html) => {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
     try {
         const info = await tranporter.sendMail({
-            from: `chiraggreed02@gmail.com ${process.env.GOOGLE_USER}`,
+            from: `AskBase: ${process.env.GOOGLE_USER}`,
             to,
             subject,
             html
         })
     }
-    catch (err) {
-        console.log(`Error sending email to ${to} `, err)
+    catch (error) {
+        console.error('Error sending email:', error);
+        if (error.response) {
+            console.error(error.response.body);
+        }
     }
 
 }
